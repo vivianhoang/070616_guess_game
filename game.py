a45c53cf5d8d987d9152c67e24c605ab88a5287d
Hackbright Student <info@hackbrightacademy.com>
2016-07-06 12:39:57 -0700

    Computer generating a random number

--- game.py
+++ game.py
@@ -1,4 +1,9 @@
+from random import randint
+
 print "Hello! Let's play a game"# Put your code here
 
 name = raw_input("What is your name?")
+random_number = randint(0, 100)
+print random_number
 
+# Guessed_num = raw_input("")
