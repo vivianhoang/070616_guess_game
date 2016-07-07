3fb03a3268d21ec3019200625d3f1320d6d4aa9d
Hackbright Student <info@hackbrightacademy.com>
2016-07-06 17:04:26 -0700

    Added while loop for testing and adding winning statements

--- game.py
+++ game.py
@@ -6,6 +6,20 @@ name = raw_input("What is your name?")
 random_number = randint(0, 100)
 #print random_number
 
-Guessed_num = raw_input("Please guess a number between 1 and 100: ")
+count = 0
 
-print Guessed_num
+while True:
+    guessed_num = raw_input("Please guess a number between 1 and 100: ")
+    guessed_num = int(guessed_num)
+    #print guessed_num
+    if guessed_num > random_number:
+        count = count + 1
+        print "Your guess is too high, try again."
+    elif guessed_num < random_number:
+        count = count + 1
+        print "Your guess is too low, try again."
+    else:
+        count = count + 1
+        print "Congratuations, {0} you found my number in {1} tries.".format(name, str(count))
+        break
+
